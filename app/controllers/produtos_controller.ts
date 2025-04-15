6b4e0c765b54dfe4e97554d2c1a0e286d2831ea3
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-04-15 05:34:43 -0300

    paginação no endpoint de produtos

--- app/controllers/produtos_controller.ts
+++ app/controllers/produtos_controller.ts
@@ -2,9 +2,24 @@ import { HttpContext } from '@adonisjs/core/http'
 import Produto from '#models/produto'
 
 export default class ProdutosController {
-  async index({ response }: HttpContext) {
-    const produtos = await Produto.query().preload('categoria')
-    return response.json(produtos)
+  async index({ request, response }: HttpContext) {
+    try {
+      const page = request.input('page', 1)
+      const limit = request.input('limit', 10)
+      if (limit > 30) {
+        return response.status(400).json({
+          message: 'O limite máximo de produtos por página é 30',
+        })
+      }
+
+      const produtos = await Produto.query().preload('categoria').paginate(page, limit)
+
+      return response.json(produtos)
+    } catch (error) {
+      return response.status(500).json({
+        message: 'Erro ao buscar produtos',
+      })
+    }
   }
 
   async show({ params, response }: HttpContext) {
