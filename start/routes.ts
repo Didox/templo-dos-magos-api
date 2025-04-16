fef5494e388ca23c328730ef568bb64b7418c5cf
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-04-16 07:28:49 -0300

    prettier

--- start/routes.ts
+++ start/routes.ts
@@ -4,36 +4,46 @@ import router from '@adonisjs/core/services/router'
 router.get('/', '#controllers/home_controller.index')
 
 // Rotas de produtos
-router.group(() => {
+router
+  .group(() => {
     router.get('/', '#controllers/produtos_controller.index').as('produtos.index')
     router.get('/:id', '#controllers/produtos_controller.show')
-}).prefix('/produtos')
+  })
+  .prefix('/produtos')
 
 // Rotas de categorias
-router.group(() => {
+router
+  .group(() => {
     router.get('/', '#controllers/categorias_controller.index').as('categorias.index')
     router.get('/:id', '#controllers/categorias_controller.show')
-}).prefix('/categorias')
+  })
+  .prefix('/categorias')
 
 // Rotas de usuários
-router.group(() => {
+router
+  .group(() => {
     router.get('/', '#controllers/usuarios_controller.index').as('usuarios.index')
     router.get('/:id', '#controllers/usuarios_controller.show')
     router.post('/', '#controllers/usuarios_controller.create')
     router.put('/:id', '#controllers/usuarios_controller.update')
     router.patch('/:id/senha', '#controllers/usuarios_controller.updateSenha')
-}).prefix('/usuarios')
+  })
+  .prefix('/usuarios')
 
 // Rotas de pedidos
-router.group(() => {
+router
+  .group(() => {
     router.get('/', '#controllers/pedidos_controller.index').as('pedidos.index')
     router.get('/:id', '#controllers/pedidos_controller.show')
     router.post('/', '#controllers/pedidos_controller.create')
     router.put('/:id', '#controllers/pedidos_controller.update')
-}).prefix('/pedidos')
+  })
+  .prefix('/pedidos')
 
 // Rotas de autenticação
-router.group(() => {
-  router.post('/login', '#controllers/auth_controller.login')
-  router.get('/me', '#controllers/auth_controller.me')
-}).prefix('/auth')
+router
+  .group(() => {
+    router.post('/login', '#controllers/auth_controller.login')
+    router.get('/me', '#controllers/auth_controller.me')
+  })
+  .prefix('/auth')
