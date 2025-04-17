7c4b35f38066b0effe03c6b3ecc6c5a5a7c4ad22
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-04-17 08:07:58 -0300

    tentativa de gerar swagger json em prod

--- start.sh
+++ start.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Carrega as variáveis do arquivo .env
-if [ -f .env ]; then
-    export $(cat .env | grep -v '^#' | xargs)
-    echo "Variáveis de ambiente carregadas com sucesso!"
-else
-    echo "Arquivo .env não encontrado!"
-    exit 1
-fi
+# # Carrega as variáveis do arquivo .env
+# if [ -f .env ]; then
+#     export $(cat .env | grep -v '^#' | xargs)
+#     echo "Variáveis de ambiente carregadas com sucesso!"
+# else
+#     echo "Arquivo .env não encontrado!"
+#     exit 1
+# fi
 
-# Verifica se as variáveis essenciais estão definidas
-if [ -z "$PORT" ] || [ -z "$HOST" ] || [ -z "$APP_KEY" ]; then
-    echo "Erro: Variáveis essenciais não encontradas no arquivo .env"
-    exit 1
-fi
+# # Verifica se as variáveis essenciais estão definidas
+# if [ -z "$PORT" ] || [ -z "$HOST" ] || [ -z "$APP_KEY" ]; then
+#     echo "Erro: Variáveis essenciais não encontradas no arquivo .env"
+#     exit 1
+# fi
 
-echo "Iniciando aplicação na porta $PORT..."
+# echo "Iniciando aplicação na porta $PORT..."
 
-npm run build
-npm start
+# npm run build
+# npm start
 
-# node ace serve --watch
+node ace serve --watch
