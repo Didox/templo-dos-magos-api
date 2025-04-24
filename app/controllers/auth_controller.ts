6163d32c39e0e5d7085fec53b83194f7210cfaee
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-04-24 06:17:54 -0300

    deixando código sem dependencia do adonis/auth utilizamos customozado

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,6 +1,5 @@
 import { HttpContext } from '@adonisjs/core/http'
 import { AuthService } from '#services/auth_service'
-import Usuario from '#models/usuario'
 import { inject } from '@adonisjs/core'
 
 @inject()
@@ -11,13 +10,5 @@ export default class AuthController {
     const { email, senha } = request.body()
     const result = await this.authService.login({ email, senha })
     return response.json(result)
-  }
-
-  async me({ auth, response }: HttpContext) {
-    const userId = (auth.user as Usuario)?.id
-    if (!userId) {
-      return response.unauthorized({ error: 'Não autorizado' })
-    }
-    return response.json(auth.user)
   }
 }
